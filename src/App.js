efee81a82103b415f5528041e6e2fbb1fe2f461f
Cyrine <boughanmycyrine@gmail.com>
2022-07-18 10:08:54 +0100

    commiting project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css"
 import Header from "./components/common/Header/Header"
-import { BrowserRouter as Router,  Route } from "react-router-dom"
-import {Routes} from "react-router"
+import { BrowserRouter as Router,Routes, Route } from "react-router-dom"
 import About from "./components/About/About"
 import CourseHome from "./components/AllCourses/CourseHome"
 import Team from "./components/Team/Team"
@@ -15,19 +14,20 @@ function App() {
     <>
       <Router>
         <Header />
+       
         <Routes>
-          <Route exact path='/' element={<Home/>} />
-          <Route exact path='/about' element={<About/>} />
-          <Route exact path='/courses' element={<CourseHome/>} />
-          <Route exact path='/team' element={<Team/>} />
-          <Route exact path='/pricing' element={<Pricing/>} />
-          <Route exact path='/journal' element={<Blog/>} />
-          <Route exact path='/contact' element={<Contact/>} />
-        </Routes>
+          <Route  path='/' element={<Home/>} />
+          <Route  path='/about' element={<About/>} />
+          <Route  path='/courses' element={<CourseHome/>} />
+          <Route  path='/team' element={<Team/>} />
+          <Route  path='/pricing' element={<Pricing/>} />
+          <Route  path='/journal' element={<Blog/>} />
+          <Route path='/contact' element={<Contact/>} />
+  </Routes>
         <Footer />
       </Router>
     </>
   )
 }
 
-export default App
+export default App;
